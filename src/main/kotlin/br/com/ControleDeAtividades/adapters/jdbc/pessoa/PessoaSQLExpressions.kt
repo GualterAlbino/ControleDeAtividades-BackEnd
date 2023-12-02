09accf3ebed18ce92979ff0823fad70bd9003219
Gualter Albino <gualteralbino1000@gmail.com>
2023-12-02 18:59:09 -0300

    Entidade Pessoas.

--- src/main/kotlin/br/com/ControleDeAtividades/adapters/jdbc/pessoa/PessoaSQLExpressions.kt
+++ src/main/kotlin/br/com/ControleDeAtividades/adapters/jdbc/pessoa/PessoaSQLExpressions.kt
@@ -1,4 +1,35 @@
 package br.com.ControleDeAtividades.adapters.jdbc.pessoa
 
 object PessoaSQLExpressions {
+    fun sqlSelectAllPessoa() = "SELECT * FROM pessoa".trimIndent()
+
+
+    fun sqlSelectByIdPessoa() = "SELECT * FROM pessoa WHERE id = :id".trimIndent()
+
+
+    fun sqlInsertPessoa() = """
+      INSERT INTO pessoa (cpf, email, rg, nome, academico, atividade_execucao, nome_mae, nome_pai, tel_residencial, tel_recado, tel_celular)
+      VALUES (:cpf, :email, :rg, :nome, :academico, :atividade_execucao, :nome_mae, :nome_pai, :tel_residencial, :tel_recado, :tel_celular);
+
+    """.trimIndent()
+
+    fun sqlDeletePessoaByCpf() = """
+        DELETE FROM pessoa WHERE cpf = :cpf
+    """.trimIndent()
+
+
+    fun sqlUpdatePessoa() = """UPDATE usuario
+        SET
+        cpf = :cpf,
+        email = :email,
+        rg = :rg,
+        nome = :nome,
+        academico = :academico,
+        atividade_execucao = :atividade_execucao,
+        nome_mae = :nome_mae,
+        nome_pai = :nome_pai,
+        tel_residencial = :tel_residencial,
+        tel_recado = :tel_recado,
+        tel_celular = :tel_celular     
+    """.trimIndent()
 }
--- src/main/kotlin/br/com/ControleDeAtividades/application/pessoa/PessoaCreateComand.kt
+++ src/main/kotlin/br/com/ControleDeAtividades/application/pessoa/PessoaCreateComand.kt
@@ -1,2 +1,35 @@
 package br.com.ControleDeAtividades.application.pessoa
 
+import br.com.ControleDeAtividades.domain.pessoa.Pessoa
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class PessoaCreateComand(
+        val cpf :String,
+        val email: String,
+        val rg: String,
+        val nome: String,
+        val academico: String,
+        val atividade_execucao: Boolean,
+        val nome_mae: String,
+        val nome_pai: String,
+        val tel_residencial: String,
+        val tel_recado: String,
+        val tel_celular: String
+)
+
+fun PessoaCreateComand.toPessoa() = Pessoa (
+        cpf = cpf,
+        email = email,
+        rg = rg,
+        nome = nome,
+        academico = academico,
+        atividadeExecucao = atividade_execucao,
+        nome_mae = nome_mae,
+        nome_pai = nome_pai,
+        tel_residencial = tel_residencial,
+        tel_recado = tel_recado,
+        tel_celular = tel_celular
+)
+
